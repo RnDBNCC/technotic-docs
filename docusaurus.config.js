6a25584089f4de172c6b4e475d8b9e7464c452ee
joshuajevon <joshuajevon05@gmail.com>
2023-03-08 23:46:39 +0700

    [add]: add contributors section (on prog)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -126,7 +126,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Bina Nusantara Computer Club, All Right Reserved.`,
       },
       prism: {
         theme: lightCodeTheme,
--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -2,34 +2,73 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
 
-const FeatureList = [
+// const FeatureList = [
+//   {
+//     title: 'Easy to Use',
+//     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+//     description: (
+//       <>
+//         Docusaurus was designed from the ground up to be easily installed and
+//         used to get your website up and running quickly.
+//       </>
+//     ),
+//   },
+//   {
+//     title: 'Focus on What Matters',
+//     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+//     description: (
+//       <>
+//         Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
+//         ahead and move your docs into the <code>docs</code> directory.
+//       </>
+//     ),
+//   },
+//   {
+//     title: 'Powered by React',
+//     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+//     description: (
+//       <>
+//         Extend or customize your website layout by reusing React. Docusaurus can
+//         be extended while reusing the same header and footer.
+//       </>
+//     ),
+//   },
+// ];
+
+const ContributorsList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    name: 'Jonathan Christian',
+    Svg: require('@site/static/img/RnDManager-JonathanChristian.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Research and Development 34 Manager
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    name: 'Joshua Jevon Irawan',
+    Svg: require('@site/static/img/RnDStaff-JoshuaJevonIrawan.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Research and Development 34 Staff
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    name: 'Rifian Fernando',
+    Svg: require('@site/static/img/RnDStaff-RifianFernando.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Research and Development 34 Staff
+      </>
+    ),
+  },
+  {
+    name: 'Defario Fitriano',
+    Svg: require('@site/static/img/RnDStaff-DefarioFitriano.svg').default,
+    description: (
+      <>
+        Research and Development 34 Staff
       </>
     ),
   },
@@ -49,13 +88,33 @@ function Feature({Svg, title, description}) {
   );
 }
 
+function Contributors({Svg, name, description}){
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{name}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
 export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
       <div className="container">
-        <div className="row">
+        <h1 className="text--center">Contributors</h1>
+        {/* <div className="row">
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
+          ))}
+        </div> */}
+        <div className="row">
+          {ContributorsList.map((props, idx) => (
+            <Contributors key={idx} {...props} />
           ))}
         </div>
       </div>
